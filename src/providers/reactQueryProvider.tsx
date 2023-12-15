21b003925152ba5738a55daefc8e76cf2e0eecf2
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-12-16 01:32:58 +0330

    create reactQueryProvider

---
+++ src/providers/reactQueryProvider.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
+export const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
+	return (
+		<QueryClientProvider client={queryClient}>
+			{children}
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
+};
