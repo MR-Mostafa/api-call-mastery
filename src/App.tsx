80f45151bdc3e80b89956eea45530bd3eacf7354
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-12-16 01:33:31 +0330

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,16 @@
 import { RouterProvider } from 'react-router-dom';
 
-import { ColorSchemeScript, MantineProvider, localStorageColorSchemeManager } from '@mantine/core';
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-import { theme } from '~/config/theme';
+import { MantineProvider } from '~/providers/mantineProvider';
+import { ReactQueryProvider } from '~/providers/reactQueryProvider';
 import { router } from '~/router';
-
-const queryClient = new QueryClient();
-
-const colorSchemeManager = localStorageColorSchemeManager({
-	key: 'color-scheme',
-});
 
 const App = () => {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<ColorSchemeScript defaultColorScheme='dark' />
-			<MantineProvider
-				defaultColorScheme='dark'
-				colorSchemeManager={colorSchemeManager}
-				theme={theme}
-			>
+		<ReactQueryProvider>
+			<MantineProvider>
 				<RouterProvider router={router()} />
 			</MantineProvider>
-			<ReactQueryDevtools initialIsOpen={false} />
-		</QueryClientProvider>
+		</ReactQueryProvider>
 	);
 };
 
